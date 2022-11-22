de27f6e41b9f8def36251e7353920c3ea83986f2
Carlos <carlos200012-@hotmail.com>
2022-11-22 15:23:19 -0500

    cuarto envio

--- classes/Email.php
+++ classes/Email.php
@@ -36,7 +36,7 @@ class Email{
 
         $contenido = "<html>";
         $contenido .= "<p> <strong> Hola ". $this->nombre ." </strong> Has creado tu cuenta en app Salón, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .="<p>Presione aquí: <a href='http://localhost:3000/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a></p>";
+        $contenido .="<p>Presiona aquí: <a href='" . $_ENV['SERVER_HOST'] . "confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .="</html>";
         $mail->Body=$contenido;
@@ -63,7 +63,7 @@ class Email{
 
         $contenido = "<html>";
         $contenido .= "<p> <strong> Hola ". $this->nombre ." </strong> Has solicitado restablecer tu password, sigue el siguiente enlace para hacerlo </p>";
-        $contenido .="<p>Presione aquí: <a href='http://localhost:3000/recuperar?token=".$this->token."'>Restablecer Password</a></p>";
+        $contenido .="<p>Presiona aquí: <a href='" . $_ENV['SERVER_HOST'] . "confirmar-cuenta?token=".$this->token."'>Restablecer Password</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .="</html>";
         $mail->Body=$contenido;
